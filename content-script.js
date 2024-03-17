75001a21476ceaff118ed590184243ab4d64b433
ptr <petar0golubovic@gmail.com>
2024-03-17 14:59:51 +0100

    Finished the first version, it includes dark mode now, which is remembered with chrome.storage.local. Also managed to remove the background of the search-less messages.

--- content-script.js
+++ content-script.js
@@ -1,4 +1,18 @@
-const removeDisclaimers = () => {
+let TOGGLE;
+
+async function getJSONs() {
+    let darkModeUrl = chrome.runtime.getURL("dark-mode-css-properties.json");
+    let darkRes = await fetch(darkModeUrl);
+    let dark = await darkRes.json();
+
+    let lightModeUrl = chrome.runtime.getURL("light-mode-css-properties.json");
+    let lightRes = await fetch(lightModeUrl);
+    let light = await lightRes.json();
+
+    return [dark, light];
+}
+
+const removeDisclaimersAtEndNoSearch = () => {
     const traverseShadowDOM = (element) => {
         if (element && element.shadowRoot) {
             const nestedElement = element.shadowRoot.querySelector(
@@ -9,9 +23,9 @@ const removeDisclaimers = () => {
                 nestedElement.remove();
             } else {
                 const childElements = element.shadowRoot.querySelectorAll("*");
-                childElements.forEach((child) => {
+                for (let child of childElements) {
                     traverseShadowDOM(child);
-                });
+                }
             }
         }
     };
@@ -20,7 +34,7 @@ const removeDisclaimers = () => {
     traverseShadowDOM(rootElement);
 };
 
-const removeDisclaimersAtBeginning = () => {
+const removeDisclaimersAtBeginningNoSearch = () => {
     const traverseShadowDOM = (element) => {
         if (element && element.shadowRoot) {
             const nestedElement =
@@ -33,9 +47,9 @@ const removeDisclaimersAtBeginning = () => {
                 nestedElement.remove();
             } else {
                 const childElements = element.shadowRoot.querySelectorAll("*");
-                childElements.forEach((child) => {
+                for (let child of childElements) {
                     traverseShadowDOM(child);
-                });
+                }
             }
         }
     };
@@ -51,22 +65,31 @@ const addThemeToggle = () => {
             if (nestedElement) {
                 let toggleDiv = document.createElement("div");
                 toggleDiv.setAttribute("style", "transform: translateX(20%)");
+                toggleDiv.setAttribute("class", "dark-mode-toggle");
                 let toggle = document.createElement("input");
+                TOGGLE = toggle;
                 toggle.setAttribute("is", "cib-switch");
                 toggle.setAttribute("class", "switch");
                 toggle.setAttribute("role", "switch");
                 toggle.setAttribute("id", "theme-switch");
-                toggle.addEventListener("change", (e) => {
-                    console.log("clicked on the toggle!");
-                    console.log(e);
+                toggle.toggleAttribute("checked");
+                toggle.addEventListener("change", () => {
+                    if (toggle.checked) {
+                        console.log("the theme should be dark now");
+                        toggle.toggleAttribute("checked");
+                        toggleTheme("dark");
+                    } else {
+                        toggle.toggleAttribute("checked");
+                        toggleTheme("light");
+                    }
                 });
-
                 toggleDiv.appendChild(toggle);
                 nestedElement.appendChild(toggleDiv);
             } else {
                 const childElements = element.shadowRoot.querySelectorAll("*");
                 childElements.forEach((child) => {
                     traverseShadowDOM(child);
+                    return;
                 });
             }
         }
@@ -76,8 +99,56 @@ const addThemeToggle = () => {
     traverseShadowDOM(rootElement);
 };
 
+async function toggleTheme(theme) {
+    let [darkMode, lightMode] = await getJSONs();
+    if (theme === "dark") {
+        let root = document.querySelector(":root");
+        let styles = getComputedStyle(document.documentElement);
+
+        for (let key of Object.keys(darkMode)) {
+            if (styles.getPropertyValue(key)) {
+                root.style.setProperty(key, darkMode[key]);
+            }
+        }
+        chrome.storage.local.set({ theme: "dark" });
+        changeScrollbarColor("#141414", "#555555");
+    } else {
+        let root = document.querySelector(":root");
+        let styles = getComputedStyle(document.documentElement);
+
+        for (let key of Object.keys(lightMode)) {
+            if (styles.getPropertyValue(key)) {
+                root.style.setProperty(key, lightMode[key]);
+            }
+        }
+        chrome.storage.local.set({ theme: "light" });
+        changeScrollbarColor("#F7F7F7", "#555555");
+    }
+}
+
+function changeScrollbarColor(track, thumb) {
+    document.body.style.scrollbarTrackColor = track;
+}
+
+async function setThemeOnOpen() {
+    let { theme } = await chrome.storage.local.get("theme");
+    if (theme === "dark") {
+        toggleTheme(theme);
+    }
+}
+
+function removeBackgroundTextureNoSearch() {
+    document
+        .querySelector(":root")
+        .style.setProperty("--cib-no-bing-result-texture", "");
+}
+
 setTimeout(() => {
-    setInterval(removeDisclaimers, 500);
-    setInterval(removeDisclaimersAtBeginning, 500);
+    setInterval(removeDisclaimersAtEndNoSearch, 500);
+    setInterval(removeDisclaimersAtBeginningNoSearch, 500);
+    removeBackgroundTextureNoSearch();
     addThemeToggle();
+    setThemeOnOpen();
 }, 500);
+
+// const darkModeString =
